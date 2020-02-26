3dc06cb0165e350bf88af122947e262a02a56658
Omac092627 <oogunde0413@gmail.com>
2020-02-25 20:51:23 -0800

    added a few files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-'use strict';
 
 var questionList = [];
 
---
+++ js/index.js
@@ -0,0 +1,4 @@
+'use strict'
+//add functionality to store name in local storage//
+var newEl = document.getElementById('user-form');
+newEl.addEventListener('submit', handleSubmit);
