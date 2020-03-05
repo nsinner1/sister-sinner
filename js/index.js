db09c2542c11d94950a18a432e31808739687ff8
Adam Owada <adamowada@gmail.com>
2020-03-04 20:14:14 -0800

    found more code to clean on index.js

--- js/index.js
+++ js/index.js
@@ -47,17 +47,22 @@ function getFromLocalStorage(keyname) {
 var slideIndex = 1;
 showDivs(slideIndex);
 
+// Operates image slides on index.html
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var x = document.getElementsByClassName('mySlides');
+  if (n > x.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex-1].style.display = 'block';
 }
